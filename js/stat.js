59f8efbf1f5d7c0f9e4c9a517c8a5a89e6eef540
Alexandr <stwork87@gmail.com>
2017-01-26 10:34:35 +0300

    changing place for histogram

--- js/stat.js
+++ js/stat.js
@@ -1,40 +1,32 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-
-
+  var histHeight = 150;// высота гистограммы
+  var histX = 100;// отступ слева для гистограммы
+  var columnWidth = 40;// ширина колонки гистограммы
+  var columnIndent = 50;// интервал между колонками
+  var step = histHeight / maxTime;
   // рисуем тень
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
-
   // рисуем облако
   ctx.fillStyle = 'rgba(255, 255, 255, 1)';
   ctx.fillRect(100, 10, 420, 270);
-
   // выводим текст шапки облака
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура, вы победили!', 50, 30);
-  ctx.fillText('Список результатов: ', 50, 50);
-
+  ctx.fillText('Ура, вы победили!', histX, 30);
+  ctx.fillText('Список результатов: ', histX, 50);
   var maxTime = -1;
   for (var z = 0; z < times.length; z++) {
     if (times[z] > maxTime) {
       maxTime = times[z];
     }
   }
-
-  var histHeight = 150;// высота гистограммы
-  var histX = 60;// отступ слева для гистограммы
-  var columnWidth = 40;// ширина колонки гистограммы
-  var columnIndent = 50;// интервал между колонками
-  var step = histHeight / maxTime;
-
   for (var i = 0; i < times.length; i++) {
     var name = names[i];
     var time = times[i];
     var columnHeight = step * time;
-
     // выводим результаты игроков
     ctx.fillText(time.toFixed(0), histX + i * (columnIndent + columnWidth), 70 + (histHeight - columnHeight));
 
@@ -45,10 +37,10 @@ window.renderStatistics = function (ctx, names, times) {
     }
     // вывод столбцов гистограммы
     ctx.fillRect(histX + i * (columnIndent + columnWidth), 80 + (histHeight - columnHeight), columnWidth, columnHeight);
-
     ctx.fillStyle = '#000';
     // вывод подписей к столбцам
     ctx.fillText(name, histX + i * (columnIndent + columnWidth), 100 + histHeight);
   }
 
 };
+
