1292010539e1d31b8be98429f5610ed46924adfe
Левшенко Александр < >
2017-02-13 12:01:57 -0800

    repair mistakes

--- js/setup.js
+++ js/setup.js
@@ -1,34 +1,16 @@
 'use strict';
 
-var openSetupWindow = document.querySelector('.setup-open');
+var openSetup = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
-var closeSetupWindow = document.querySelector('.setup-close');
+var closeSetup = document.querySelector('.setup-close');
 var setupOverlay = document.querySelector('.overlay');
-// одежда
 var wizardCoat = document.getElementById('wizard-coat');
-// глаза
 var wizardEyes = document.getElementById('wizard-eyes');
-// мячик
 var wizardFireboll = document.querySelector('.setup-fireball-wrap');
-// кнопка "Сохранить"
-// var buttonSetupSubmit = document.querySelector('.setup-submit');
 
 var ENTER_KEY_CODE = 13;
 var ESC_KEY_CODE = 27;
 
-function isActivateEvent(evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-}
-
-function hideSetupElement() {
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode && evt.keyCode === ESC_KEY_CODE) {
-      setupOverlay.classList.add('invisible');
-    }
-  });
-}
-
-// массив значений цвета одежды
 var colorWizardCoat = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
@@ -36,14 +18,14 @@ var colorWizardCoat = [
   'rgb(56, 159, 117)',
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'];
-// массив значений цвета глаз
+
 var colorWizardEyes = [
   'black',
   'red',
   'blue',
   'yellow',
   'green'];
-// массив значений цвета мячика
+
 var colorWizardFireboll = [
   '#ee4830',
   '#30a8ee',
@@ -51,73 +33,92 @@ var colorWizardFireboll = [
   '#e848d5',
   '#e6e848'];
 
-// обработчик клика на аватарку
-openSetupWindow.addEventListener('click', function () {
-  setup.classList.remove('invisible');
-  hideSetupElement();
-});
+openSetup.addEventListener('click', open);
+openSetup.addEventListener('keydown', openByEnter);
+closeSetup.addEventListener('click', close);
+closeSetup.addEventListener('keydown', closeByEsc);
+wizardCoat.addEventListener('click', getWizardCoatColor);
+wizardCoat.addEventListener('keydown', getWizardCoatColorByEnter);
+wizardEyes.addEventListener('click', getWizardEyesColor);
+wizardEyes.addEventListener('keydown', getWizardEyesColorByEnter);
+wizardFireboll.addEventListener('click', getWizardFirebollColor);
+wizardFireboll.addEventListener('keydown', getWizardFirebollColorByEnter);
+document.addEventListener('keydown', hideSetupElement);
 
-// обработчик клика на аватаркус клавиатуры
-openSetupWindow.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    setup.classList.remove('invisible');
-    hideSetupElement(evt);
-  }
-});
-
-// обработчик клика на кнопку "Закрыть"
-closeSetupWindow.addEventListener('click', function () {
-  setup.classList.add('invisible');
-});
-
-// обработчик клика на кнопку "Закрыть" с клавиатуры
-closeSetupWindow.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    setup.classList.add('invisible');
-  }
-});
-
-// обработчик клика на одежду
-wizardCoat.addEventListener('click', function () {
-  wizardCoat.style.fill = colorWizardCoat[(Math.random() * colorWizardCoat.length).toFixed(0)];
-});
-
-// обработчик клика на одежду с клавиатуры
-wizardCoat.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    wizardCoat.style.fill = colorWizardCoat[(Math.random() * colorWizardCoat.length).toFixed(0)];
-    tooglingAria(wizardCoat);
-  }
-});
-
-// обработчик клика на глаза
-wizardEyes.addEventListener('click', function () {
-  wizardEyes.style.fill = colorWizardEyes[(Math.random() * colorWizardEyes.length).toFixed(0)];
-});
-
-// обработчик клика на глаза с клавиатуры
-wizardEyes.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    wizardEyes.style.fill = colorWizardEyes[(Math.random() * colorWizardEyes.length).toFixed(0)];
-    tooglingAria(wizardEyes);
-  }
-});
-
-// обработчик клика на мячик
-wizardFireboll.addEventListener('click', function () {
-  wizardFireboll.style.backgroundColor = colorWizardFireboll[(Math.random() * colorWizardFireboll.length).toFixed(0)];
-});
-
-// обработчик клика на мячик с клавиатуры
-wizardFireboll.addEventListener('keydown', function (evt) {
-  if (isActivateEvent(evt)) {
-    wizardFireboll.style.backgroundColor = colorWizardFireboll[(Math.random() * colorWizardFireboll.length).toFixed(0)];
-    tooglingAria(wizardFireboll);
-  }
-});
-
-// toogling aria-checked
-function tooglingAria(label) {
+function togglingAria(label) {
   var pressed = (label.getAttribute('aria-pressed') === 'true');
   label.setAttribute('aria-pressed', !pressed);
+}
+
+function isActivateEvent(evt) {
+  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+}
+
+function isEscapeEvent(evt) {
+  return evt.keyCode && evt.keyCode === ESC_KEY_CODE;
+}
+
+function hideSetupElement(evt) {
+  if (isEscapeEvent) {
+    setupOverlay.classList.add('invisible');
+  }
+}
+
+function closeByEsc(evt) {
+  if (isActivateEvent(evt)) {
+    close();
+  }
+}
+
+function open() {
+  setup.classList.remove('invisible');
+  openSetup.setAttribute('aria-pressed', 'true');
+}
+
+function openByEnter(evt) {
+  if (isActivateEvent(evt)) {
+    open();
+  }
+}
+
+function close() {
+  setup.classList.add('invisible');
+  openSetup.setAttribute('aria-pressed', 'false');
+}
+
+function getRandomColor(element, property, array) {
+  var color = array[(Math.random() * array.length).toFixed(0)];
+  element.style[property] = color;
+}
+
+function getWizardCoatColor() {
+  getRandomColor(wizardCoat, 'fill', colorWizardCoat);
+}
+
+function getWizardCoatColorByEnter(evt) {
+  if (isActivateEvent(evt)) {
+    getRandomColor(wizardCoat, 'fill', colorWizardCoat);
+    togglingAria(wizardCoat);
+  }
+}
+
+function getWizardEyesColor() {
+  getRandomColor(wizardEyes, 'fill', colorWizardEyes);
+}
+function getWizardEyesColorByEnter(evt) {
+  if (isActivateEvent(evt)) {
+    getRandomColor(wizardEyes, 'fill', colorWizardEyes);
+    togglingAria(wizardEyes);
+  }
+}
+
+function getWizardFirebollColor() {
+  getRandomColor(wizardFireboll, 'backgroundColor', colorWizardFireboll);
+}
+
+function getWizardFirebollColorByEnter(evt) {
+  if (isActivateEvent(evt)) {
+    getRandomColor(wizardFireboll, 'backgroundColor', colorWizardFireboll);
+    togglingAria(wizardFireboll);
+  }
 }
